6306f02634d65525d950a631ababcf7b59155004
gregology <greg@gho.st>
2012-07-30 20:44:22 +1000

    added licence details

--- index.php
+++ index.php
@@ -1,13 +1,34 @@
 <?php
-session_start();
+/*
+    Anon Ghost is a secure communication medium for those in liberty voids.
+    Masks are generated automatically with the users details or with a
+    mask-seed to create private identities so users can create trusted
+    relationships.
+    Check out http://anon.gho.st for a working version.
+    Copyright (C) 2012 Gregology
 
-// Bad-bahavior script
-$path_to_bb = '/var/www/bad-behavior';
-require_once("$path_to_bb/bad-behavior-generic.php");
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+session_start();
 
 // Includes the settings from the config file
 include("config.php");
 include("functions.php");
+
+// Optional Bad-bahavior script
+// require_once("$path_to_bb/bad-behavior-generic.php");
 
 // Opens database connections
 $conn = mysql_connect($dbhost, $dbuser, $dbpass) or die ('Error connecting to mysql');
